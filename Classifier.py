69f3f11f3f4597c74ab49bf04bd1117b7c994ea4
Jacob Turner <jacob11turner@gmail.com>
2021-04-30 10:50:13 +0100

    Add progress saving for all data

--- Classifier.py
+++ Classifier.py
@@ -2,6 +2,7 @@ import sys
 import os
 import time
 import torch
+import pickle
 import argparse
 import matplotlib.pyplot as plt
 
@@ -16,15 +17,18 @@ parser.add_argument('-t', action='store_true')
 parser.add_argument('-r', action='store_true')
 args = parser.parse_args()
 PATH = f"{sys.path[0]}\\cifar_net.pth"
+PROGRESS_DATA = f"{sys.path[0]}\\progress.bin"
+PROGRESS_GRAPH = f"{sys.path[0]}\\plot.png"
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 MAX_EPOCHS = 500
 BATCH_SIZE = 64
 CLASSES = ("plane", "car", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck")
 
 class Net(nn.Module):
+
     def __init__(self):
         super().__init__()
-        self.features = self.createLayers()
+        self.features = self.createLayers([(3, 32, 2, 0.2), (32, 64, 2, 0.3), (64, 128, 3, 0.4), (128, 256, 3, 0.4)])
 
     def forward(self, x):
         x = self.features(x)
@@ -41,9 +45,9 @@ class Net(nn.Module):
         items.append(nn.Dropout(p=dropout))
         return items
     
-    def createLayers(self):
+    def createLayers(self, design):
         layers = []
-        for param in [(3, 32, 2, 0.2), (32, 64, 2, 0.3), (64, 128, 3, 0.4), (128, 256, 3, 0.4)]:
+        for param in design:
             layers.extend(self.createConvLayer(*param))
         layers.extend([
             nn.Flatten(),
@@ -86,6 +90,7 @@ testloader = torch.utils.data.DataLoader(testset, batch_size=BATCH_SIZE, shuffle
 
 net = Net().to(DEVICE)
 
+learningRate = 1e-3
 trainAccuracyList = [0]
 testAccuracyList = [0]
 lossRates = [1]
@@ -93,33 +98,42 @@ epochs = [0]
 fig = plt.figure()
 ax = plt.subplot()
 plt.ylim([0, 100])
-ax.plot(epochs, trainAccuracyList, color="blue", label="Train Accuracy (%)")
-ax.plot(epochs, testAccuracyList, color="orange", label="Test Accuracy (%)")
-ax.plot(epochs, lossRates, color="green", label="loss (100 * (1-loss))")
-plt.legend(loc="upper left")
-fig.savefig('plot.png')
 
-if args.r:
-    try: os.remove(PATH)
-    except: pass
 
 def adjustLearningRate(optimizer):
-    lr = 1e-3 * 0.993
     for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
+        param_group['lr'] *= 0.993
 
+if args.r:
+    for f in [PATH, PROGRESS_DATA, PROGRESS_GRAPH]:
+        try: os.remove(f)
+        except: pass
 
 if os.path.isfile(PATH):
     net.load_state_dict(torch.load(PATH))
+    with open(PROGRESS_DATA, "rb") as binFile:
+        learningRate = pickle.load(binFile)
+        trainAccuracyList = pickle.load(binFile)
+        testAccuracyList = pickle.load(binFile)
+        lossRates = pickle.load(binFile)
+        epochs = pickle.load(binFile)
     print(f"Loaded state from: {PATH}")
+else:
+    print("No load state. Creating new model.")
 if args.t or not os.path.isfile(PATH):
+    ax.plot(epochs, trainAccuracyList, color="blue", label="Train Accuracy (%)")
+    ax.plot(epochs, testAccuracyList, color="orange", label="Test Accuracy (%)")
+    ax.plot(epochs, lossRates, color="green", label="loss (100 * (1-loss))")
+    plt.legend(loc="upper left")
+    fig.savefig(PROGRESS_GRAPH)
+
     trainingStart = time.time()
-    bestAccuracy = 0
-    print("No load state found, beginning training...")
+    bestAccuracy = max(testAccuracyList)
+    print("Beginning training...")
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(net.parameters(), lr=1e-3)
-    # optimizer = optim.Adam(net.parameters())
-    for epoch in range(MAX_EPOCHS):
+    optimizer = torch.optim.Adam(net.parameters(), lr=learningRate)
+
+    for epoch in range(epochs[-1]+1, MAX_EPOCHS):
         start = time.time()
         for i, data in enumerate(trainloader):
             inputs, labels = data[0].to(DEVICE), data[1].to(DEVICE)
@@ -135,13 +149,19 @@ if args.t or not os.path.isfile(PATH):
         trainAccuracyList.append(trainAccuracy)
         testAccuracyList.append(accuracy)
         lossRates.append(100 * (1-loss.item()))
-        epochs.append(epoch+1)
+        epochs.append(epoch)
         ax.plot(epochs, trainAccuracyList, color="blue", label="Train Accuracy (%)")
         ax.plot(epochs, testAccuracyList, color="orange", label="Test Accuracy (%)")
         ax.plot(epochs, lossRates, color="green", label="loss")
-        fig.savefig('plot.png')
+        fig.savefig(PROGRESS_GRAPH)
+        with open(PROGRESS_DATA, "wb") as binFile:
+            pickle.dump(learningRate, binFile)
+            pickle.dump(trainAccuracyList, binFile)
+            pickle.dump(testAccuracyList, binFile)
+            pickle.dump(lossRates, binFile)
+            pickle.dump(epochs, binFile)
 
-        print(f"Epoch {epoch+1} completed in {round(time.time() - start, 2)} seconds")
+        print(f"Epoch {epoch} completed in {round(time.time() - start, 2)} seconds")
         print(f"Train accuracy: {trainAccuracy}% Test Accuracy: {accuracy}% Loss: {loss}")
         adjustLearningRate(optimizer)
 
